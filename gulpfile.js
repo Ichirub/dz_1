5849d55f00574193b2a4e9d522d87862a08c5531
Ichirub <ichirub@mail.ru>
2015-07-24 11:58:24 +0300

    Second

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
 ﻿var gulp = require("gulp"),
-	browserSync = require("browser-sync");
+    browserSync = require("browser-sync");
 
 gulp.task("server", function () {
-	browserSync({
-		port: 9000,
-		server: {
-			"baseDir": "app"
-		}
-	});
+    browserSync({
+        port: 9000,
+        server: {
+            "baseDir": "app"
+        }
+    });
 });
 
 gulp.task("watch", function () {
-	gulp.watch([
-		"app/*.html",
-		"app/js/**/*.js",
-		"app/css/**/*.css"
-	]).on("change", browserSync.reload);
+    gulp.watch([
+        "app/*.html",
+        "app/js/**/*.js",
+        "app/css/**/*.css"
+    ]).on("change", browserSync.reload);
 });
 
 gulp.task("default", ["server", "watch"]);
